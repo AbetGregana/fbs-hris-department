87f7a67cf437745d89ce8198ebda087a8573b4ac
AbetGregana <ansbert.gregana@frontlinebusiness.com.ph>
2024-04-24 15:45:35 +0800

    button disable complete

--- src/components/partials/modal/ModalSideWrapper.jsx
+++ src/components/partials/modal/ModalSideWrapper.jsx
@@ -10,7 +10,7 @@ const ModalSideWrapper = (props) => {
   return (
     <div className=" fixed top-0 left-0 h-full w-full flex justify-end z-50">
       <div
-        className=" backdrop bg-black/80 h-full w-full absolute top-0 left-0 z-[-1] "
+        className=" backdrop bg-black/60 h-full w-full absolute top-0 left-0 z-[-1] "
         onClick={handleCloseAll}
       ></div>
       {props.children}
